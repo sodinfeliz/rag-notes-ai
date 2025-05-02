5016e6da4805f5d5833fb26d1feeae11938c6d9f
Elliot Su <sodinfeliz@gmail.com>
2025-05-02 10:56:27 +0800

    feat: update configuration to use environment variables
    - Replaced hardcoded values for VAULT_DIR, OPENAI_MODEL_NAME, and EMBEDDING_MODEL_NAME with environment variable loading using dotenv.
    - Improved project configuration for better flexibility and security.

--- app/core/config.py
+++ app/core/config.py
@@ -1,15 +1,17 @@
-# Obsidian vault directory
-VAULT_DIR = "./tests"
+import os
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+# From .env
+VAULT_DIR = os.getenv("VAULT_DIR")
+OPENAI_MODEL_NAME = os.getenv("OPENAI_MODEL_NAME")
+EMBEDDING_MODEL_NAME = os.getenv("EMBEDDING_MODEL_NAME")
 
 # Index file path & sync log file
 INDEX_FILE = "ragnote.index"
 SYNC_LOG_FILE = "sync_log.json"
-
-# Embedding model name
-EMBEDDING_MODEL_NAME = "all-MiniLM-L6-v2"
-
-# OpenAI LLM model name
-OPENAI_MODEL_NAME = "gpt-4.1-mini"
 
 # Chunk settings
 CHUNK_SIZE = 500
